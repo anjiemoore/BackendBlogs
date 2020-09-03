62f22482d4fd642095d829f808a8063b952f0345
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-03 17:11:22 -0600

    added session and google oauth requirements

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const expdbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 // Database connection
 connectDB();
@@ -22,6 +27,17 @@ if(process.env.NODE_ENV === 'development') {
 // Handlebars extension name
 app.engine('.hbs', expdbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
+
+// Session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
