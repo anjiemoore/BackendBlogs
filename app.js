c20d549425afc6312bebe9db87e30ca3506e4a6d
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-03 12:24:33 -0600

    setting up database connections

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // Express Server
 
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require('express')
+const dotenv = require('dotenv')
 
 // Load config
-dotenv.config({ path: './config/config.env'});
+dotenv.config({ path: './config/config.env'})
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    )
