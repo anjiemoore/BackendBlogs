6c48a86e5ec09789fa2a4a82f1af53a6d44ccc14
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-14 16:14:34 -0600

    dashboard views and setting up story loop

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
-
+const Story = require('../models/Story')
 // Login/Landing page
 // GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -13,9 +13,17 @@ router.get('/', ensureGuest, (req, res) => {
 // Dashboard
 // GET /dashboard
 router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+        })
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
+    }
+
 })
 
 module.exports = router
