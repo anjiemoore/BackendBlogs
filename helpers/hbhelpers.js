53d0b8280e453860d673800107ef7e23372d4da9
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-18 19:36:21 -0600

    added date-fns package for later to replace momentjs and started the route for public entries

--- helpers/hbhelpers.js
+++ helpers/hbhelpers.js
@@ -1,7 +1,10 @@
 const moment = require('moment')
+const format = require('date-fns/format')
 
 module.exports = {
     formatDate: function (date, format) {
         return moment(date).format(format)
-    },
+    }
 }
+
+//September 14th, 2020 at 5:29:27pm
