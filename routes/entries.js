2e8e3fb0ce51555615498f3363520bdb21f1ffb2
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-22 15:57:54 -0600

    added separate routes to private and public entries

--- routes/entries.js
+++ routes/entries.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { ensureAuth } = require('../middleware/auth')
+const Entries = require('../models/Entries')
 const Entry = require('../models/Entries')
 
 // Show add journal entry page
@@ -16,6 +17,43 @@ router.post('/', ensureAuth, async (req, res) => {
         req.body.user = req.user.id
         await Entry.create(req.body)
         res.redirect('/dashboard')
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
+    }
+})
+
+// Show Public entries
+//GET /entries/public
+router.get('/public', ensureAuth, async (req, res) => {
+    try {
+        const entries = await Entries.find({ status: 'public' })
+        .populate('user')
+        .sort({ createdAt: 'desc' })
+        .lean()
+
+        res.render('entries/public', {
+            entries
+        })
+    } catch (error) {
+        console.error(error)
+        res.render('error/500')
+    }
+
+})
+
+// Show Private entries
+//GET /entries/private
+router.get('/private', ensureAuth, async (req, res) => {
+    try {
+        const entries = await Entries.find({ status: 'private' })
+        .populate('user')
+        .sort({ createdAt: 'desc' })
+        .lean()
+
+        res.render('entries/private', {
+            entries
+        })
     } catch (error) {
         console.error(error)
         res.render('error/500')
