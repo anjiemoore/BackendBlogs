5eaf72db28b0846a79634790c4dd1dd6979bab72
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-22 16:31:31 -0600

    turncated entries in the public/private pages and stripped of tags using regex expressions

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-const { formatDate } = require('./helpers/hbhelpers')
+const { formatDate, truncate, stripTags } = require('./helpers/hbhelpers')
 
 // Handlebars extension name
 app.engine(
     '.hbs', 
     expdbs({ 
-    helpers: {formatDate,}, 
+    helpers: {formatDate, truncate, stripTags}, 
     defaultLayout: 'main', 
     extname: '.hbs' 
     })
